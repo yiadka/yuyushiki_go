fa0b72dbd5dfc6ef770f738460c16f621474f297
Yu-kaida <moondrip4869@gmail.com>
2023-10-12 22:13:02 +0900

    First commit

---
+++ cmd/aa.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+var createCmd = &cobra.Command{
+	Use:   "aa",
+	Short: "Display ASCII art",
+	Long:  `Display ASCII art according to the command entered`,
+	RunE:  func(cmd *cobra.Command, args []string) error { return errors.New("not found command") },
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
